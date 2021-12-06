c1f29bbe67742b88c9bf78bab7675f9ce3fd5045
user <user@domain.com>
2021-12-06 12:29:19 +0200

    code refactor

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-const exec = require('child_process').execFile
-const path = require('path')
-const os = require('os')
-const fs = require('fs')
+const { execFile } = require('child_process')
+const { join } = require('path')
+const { tmpdir } = require('os')
+const { writeFile } = require('fs')
 const printer = 'printer.exe'
+
 const cors = require('cors');
 const Express = require('express')
 const server = Express()
     .use(Express.json())
     .use(cors())
-    .use((req, res, next) => { console.log(req.method, req.url); next() })
+    .use((req, res, next) => {
+        console.log(req.method, req.url);
+        next()
+    })
 
-server.get('/check', (req, res) => { res.send('true') })
+server.get('/ping', (req, res) => {
+    res.send({ ok: true })
+})
 
 server.post('/print', (req, res) => {
     const { _id, title, worker, floor, timestamp } = req.body
-    if (!_id || !title || !worker || !floor || !timestamp) {
-        return res.status(400).send({ error: 'missing id or title' })
-    }
-    try {
-        const file = path.join(os.tmpdir(), 'print', `${_id}.txt`)
+    if (!_id || !title || !worker || !floor || !timestamp)
+        return res.status(400).send({ error: 'missing details' })
 
+    try {
+        const file = join(tmpdir(), 'print', `${_id}.txt`)
         const content =
             `${formatTime(timestamp)}\n` +
             `${title}\n` +
             `עובד: ${worker} קומה ${floor}`
-        fs.writeFile(file, content, (err) => {
+
+        writeFile(file, content, (err) => {
             if (err) return res.status(500).send({ error: err.message })
-            exec(printer, [file])
+            execFile(printer, [file])
         })
     } catch (err) {
-        if (err) return res.status(500).send({ error: err.message || 'unknown error' })
+        if (err) return res.status(500)
+            .send({ error: err.message || 'error while trying to process the print request' })
     }
     res.send({ ok: true })
 })
 
 server.listen(8215, () => console.log('listening on port 8215...'))
-
 
 function formatTime(timestamp) {
     const t = new Date(timestamp)
